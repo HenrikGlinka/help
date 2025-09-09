ec4112f907ba3c392e2f7de67366e44ce24082e8
Henrik <han@rts.dk>
2025-09-09 13:23:14 +0200

    chore: Comment out push notification button in header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header({ title }) {
         <header className="text-center py-3 bg-white grid grid-cols-3">
             <h1 className="text-gray-700 mb-0 col-start-2 row-start-1">{title}</h1>
             <p className="text-sm font-light italic col-start-2 row-start-2">"Spørg om hjælp, hvis du er i tvivl!"</p>
-            <button onClick={() => subscribeUserToPush()} className="justify-self-end mr-3 text-2xl w-11 h-11 text-gray-400 hover:text-gray-600 col-start-3 row-span-2"><IoIosNotifications className="w-full h-full" /></button>
+            {/* <button onClick={() => subscribeUserToPush()} className="justify-self-end mr-3 text-2xl w-11 h-11 text-gray-400 hover:text-gray-600 col-start-3 row-span-2"><IoIosNotifications className="w-full h-full" /></button> */}
         </header>
     )
 }
