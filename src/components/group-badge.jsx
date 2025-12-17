80d82a3359a266177b80f8116d65f1e3ea1b4979
Henrik <han@rts.dk>
2025-12-17 15:17:39 +0100

    style: Add select-none class to GroupBadge and LevelBadge for improved text selection behavior

--- src/components/group-badge.jsx
+++ src/components/group-badge.jsx
@@ -1,6 +1,6 @@
 export function GroupBadge({ group }) {
 
     return (
-        <span className="text-xs text-center !font-mono font-bold text-white bg-gray-500 p-[.05rem_.2rem] rounded-sm w-min min-w-[5ch]">{group?.toUpperCase()}</span>
+        <span className="text-xs text-center !font-mono font-bold text-white bg-gray-500 p-[.05rem_.2rem] rounded-sm w-min min-w-[5ch] select-none">{group?.toUpperCase()}</span>
     );
 }
