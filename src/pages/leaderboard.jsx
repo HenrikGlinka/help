208237d8684ee7dc476d0eeadd6f6272ab5286fc
Henrik <han@rts.dk>
2025-12-19 12:26:12 +0100

    fix: Adjust table layout and styling in Leaderboard component for improved readability

--- src/pages/leaderboard.jsx
+++ src/pages/leaderboard.jsx
@@ -23,20 +23,20 @@ export default function Leaderboard() {
         <>
             <Header title="Henrik.help"></Header>
             <main>
-                <table className="border-1 text-sm [&_th,_td]:first:pl-5 [&_th,_td]:last:pr-5 [&_th,_td]:py-2.5 w-full [&_tr]:border-b bg-white dark:bg-gray-700 rounded-xl overflow-hidden">
+                <table className="border-1 text-sm [&_th,_td]:first:pl-2 [&_th,_td]:last:pr-2 [&_th,_td]:py-2.5 w-full [&_tr]:border-b bg-white dark:bg-gray-700 rounded-xl overflow-hidden">
                     <thead>
                         <tr className="bg-gray-400 dark:bg-gray-800 text-white">
-                            <th className="w-[10%]">#</th>
-                            <th colSpan={2} className="w-[70%]">Bruger</th>
-                            <th className="w-[20%] text-right">Point</th>
+                            <th className="pr-4 text-left w-[10%]">#</th>
+                            <th colSpan={2} className="w-[100%]">Bruger</th>
+                            <th className="text-right w-[0%]">Point</th>
                         </tr>
                     </thead>
                     <tbody>
                         {leaderboardData ? leaderboardData.map((user, index) => (
                             <tr key={index}>
-                                <td className="text-center">{index + 1}</td>
-                                <td className="px-4"><Link to={`/profile/${user._id}`}><UserTag username={user.username} level={getLevel(user.exp)} group={user.group} shorten /></Link></td>
-                                <td colSpan={2} className="text-right !font-mono w-0">{user.exp}</td>
+                                <td className="font-bold">{index + 1}</td>
+                                <td><Link to={`/profile/${user._id}`}><UserTag username={user.username} level={getLevel(user.exp)} group={user.group} shorten /></Link></td>
+                                <td colSpan={2} className="text-right !font-mono">{user.exp}</td>
                             </tr>
                         )) : Array.from({ length: 10 }).map((_, index) => (
                             <tr key={index} className="animate-[var(--animate-loading)]"><td className="text-center">{index + 1}</td><td className="px-4">&nbsp;</td><td className="text-right !font-mono w-0" colSpan={2}>-</td></tr>
