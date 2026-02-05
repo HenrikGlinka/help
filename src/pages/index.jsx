c02d0d5703c2f0c2efc50cdd049f0869d997f154
Henrik <han@rts.dk>
2026-02-05 14:19:54 +0100

    fix: Update package dependencies and refactor IndexPage for improved readability and functionality

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,111 +11,115 @@ import { useAlert } from "../contexts/alert-context";
 
 export default function IndexPage() {
 
-    const [tickets, setTickets] = useState(null);
-    const previousTickets = useRef([]);
     const navigate = useNavigate();
+    
+    if (localStorage.getItem('token') === null) {
+        navigate('/login');
+    } else {
 
-    const user = useLogin();
-    const alert = useAlert();
+        const [tickets, setTickets] = useState(null);
+        const previousTickets = useRef([]);
 
-    let updateInterval = useRef(null);
+        const user = useLogin();
+        const alert = useAlert();
 
+        let updateInterval = useRef(null);
 
+        const updateTickets = async () => {
 
-    const updateTickets = async () => {
-
-        if (document.hidden) {
-            return;
-        }
-
-        if (!user.isLoading && !await user.tokenIsValid()) {
-            user.logout();
-            return;
-        }
-
-        await user.update();
-
-        const requests = await getOpenRequests(user.data.group);
-
-        if (requests.error) return user.logout();
-
-        if (requests.length > 0) {
-            const newTicketIds = requests.map(req => req._id);
-            const existingTicketIds = previousTickets.current.map(ticket => ticket._id);
-            const isNewTicket = newTicketIds.some(id => !existingTicketIds.includes(id));
-
-            if (isNewTicket && localStorage.getItem('sound') !== null) {
-                const audio = new Audio(messageSound);
-                audio.play();
+            if (document.hidden) {
+                return;
             }
-        }
 
-        previousTickets.current = requests;
-        setTickets(requests);
-    }
-
-    useEffect(() => {
-        clearInterval(updateInterval.current);
-
-        if (user.isLoading) return console.log('User data is still loading...');
-
-        const checkAuthAndUpdate = async () => {
-
-            if (localStorage.getItem('token') === null || !await user.tokenIsValid()) {
+            if (!user.isLoading && !await user.tokenIsValid()) {
                 user.logout();
+                return;
+            }
+
+            await user.update();
+
+            const requests = await getOpenRequests(user.data.group);
+
+            if (requests.error) return user.logout();
+
+            if (requests.length > 0) {
+                const newTicketIds = requests.map(req => req._id);
+                const existingTicketIds = previousTickets.current.map(ticket => ticket._id);
+                const isNewTicket = newTicketIds.some(id => !existingTicketIds.includes(id));
+
+                if (isNewTicket && localStorage.getItem('sound') !== null) {
+                    const audio = new Audio(messageSound);
+                    audio.play();
+                }
+            }
+
+            previousTickets.current = requests;
+            setTickets(requests);
+        }
+
+        useEffect(() => {
+            clearInterval(updateInterval.current);
+
+            if (user.isLoading) return console.log('User data is still loading...');
+
+            const checkAuthAndUpdate = async () => {
+
+                if (localStorage.getItem('token') === null || !await user.tokenIsValid()) {
+                    user.logout();
+                } else {
+                    updateInterval.current = setInterval(updateTickets, 10000);
+                    updateTickets();
+                }
+            }
+
+            checkAuthAndUpdate();
+
+            return () => clearInterval(updateInterval.current);
+
+        }, [user.isLoading]);
+
+
+        async function askNewQuestion() {
+            alert.hide();
+            const openQuestions = await getOpenRequestsByUser();
+
+            if (openQuestions.length > 0) {
+                alert.warning("Vent venligst", "Du har allerede et åbent spørgsmål. Du kan ikke oprette et nyt spørgsmål før det nuværende er besvaret.");
             } else {
-                updateInterval.current = setInterval(updateTickets, 10000);
-                updateTickets();
+                navigate('/ask');
             }
         }
 
-        checkAuthAndUpdate();
+        if (user.isLoading) return null;
 
-        return () => clearInterval(updateInterval.current);
-
-    }, [user.isLoading]);
-
-
-    async function askNewQuestion() {
-        alert.hide();
-        const openQuestions = await getOpenRequestsByUser();
-
-        if (openQuestions.length > 0) {
-            alert.warning("Vent venligst", "Du har allerede et åbent spørgsmål. Du kan ikke oprette et nyt spørgsmål før det nuværende er besvaret.");
-        } else {
-            navigate('/ask');
-        }
+        return (
+            <>
+                <Header title="Henrik.help"></Header>
+                <main className="justify-between">
+                    <h2>Venteliste for <span className="italic">{user.data?.group.toLowerCase() === 'all' ? 'samtlige hold' : user.data?.group.toUpperCase()}</span></h2>
+                    <div className="mb-4 overflow-y-auto border-y-1 py-2">
+                        {
+                            tickets ?
+                                (tickets.length > 0 ?
+                                    tickets.map((ticketData, index) => <QueueCard key={index} ticket={ticketData} onUpdate={updateTickets} />)
+                                    : <p className="text-center text-gray-500 p-3 italic">Ventelisten er tom.</p>)
+                                :
+                                <>
+                                    <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
+                                    <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
+                                    <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
+                                </>
+                        }
+                    </div>
+                    <menu className="mt-auto">
+                        <li className="flex items-center gap-4">
+                            <button onClick={askNewQuestion} className="approve w-full"><LuMessageCircleQuestion className="mr-1" size={20} />
+                                Nyt spørgsmål
+                            </button>
+                        </li>
+                    </menu>
+                </main>
+            </>
+        )
     }
-
-    if (user.isLoading) return null;
-
-    return (
-        <>
-            <Header title="Henrik.help"></Header>
-            <main className="justify-between">
-                <h2>Venteliste for <span className="italic">{user.data?.group.toLowerCase() === 'all' ? 'samtlige hold' : user.data?.group.toUpperCase()}</span></h2>
-                <div className="mb-4 overflow-y-auto border-y-1 py-2">
-                    {
-                        tickets ?
-                            (tickets.length > 0 ?
-                                tickets.map((ticketData, index) => <QueueCard key={index} ticket={ticketData} onUpdate={updateTickets} />)
-                                : <p className="text-center text-gray-500 p-3 italic">Ventelisten er tom.</p>)
-                            :
-                            <>
-                                <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
-                                <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
-                                <Skeleton variant="rectangular" className="mb-1 rounded-2xl" height='5.75rem' />
-                            </>
-                    }
-                </div>
-                <menu className="mt-auto">
-                    <li className="flex items-center gap-4">
-                        <button onClick={askNewQuestion} className="approve w-full"><LuMessageCircleQuestion className="mr-1" size={20} />
-                            Nyt spørgsmål
-                        </button>
-                    </li>
-                </menu>
-            </main>
-        </>
-    )
 }
